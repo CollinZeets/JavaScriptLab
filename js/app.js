bb8232bf31ff36c0b3f46257c7178cc116a27096
Collin Zeets <ckzeets@gmail.com>
2020-12-16 15:54:54 -0600

    js scripting

--- js/app.js
+++ js/app.js
@@ -1,31 +1,18 @@
 'use strict';
 //object  dot operator   method/function     parameters
-console.log('this is identifier for the number: ', 5);
-
-var greeting;
-//keyword var for creating variable.
-//4 + x = 8  
-//x is the variable 4
-//prompt();
-//document.write();
-//remove();
-//Date();
-//write();
-var userName = prompt('Please enter your first name.');
+var color = prompt('Please enter your favorite color.');
 // console.log(userName);
 var today = new Date();
 // console.log(today);
-var hourNow = prompt('What is the current time of day?, Military time 0-23');
+var hourNow = prompt('What is the current time of day?');
 if(hourNow > 18 && hourNow < 24){
    greeting = 'Good Evening.';
 } else if (hourNow > 12 && hourNow < 18){
    greeting =  'Happy Afternoon.';
 } else if (hourNow < 12 && hourNow > 0 ){
   greeting = 'Good Morning';
-} else {
-    greeting = "Something went wrong.. Sorry.";
 }
 
 
 
-document.write('<h1>' + greeting + '</h1>');
+document.write('Your Favorite color is ' + color);
